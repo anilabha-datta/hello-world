0a31651d3c5a95b065ffade3024e205538c2b501
Anilabha Datta <anilabha.datta@wtwco.com>
2025-07-10 08:55:29 +0000

    fix

---
+++ MyApp/Program1.cs
@@ -0,0 +1,11 @@
+﻿namespace MyApp
+{
+    public class Program1
+    {
+        // original code from Program2.cs goes inside Main:
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World1!");
+        }
+    }
+}
---
+++ MyApp/Program2.cs
@@ -0,0 +1,12 @@
+﻿namespace MyApp
+{
+    public class Program2
+    {
+        // original code from Program2.cs goes inside Main:
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World2!");
+
+        }
+    }
+}
